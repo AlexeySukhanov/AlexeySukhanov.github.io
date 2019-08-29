766525f06aed463312f08a75c92729a850115b82
sukhanov <webdevforbiz@gmail.com>
2019-08-29 21:15:14 +0300

    BUGFIX 2

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,8 @@
 $(document).ready(function(){
 	$('.phones').prepend( '<div id="in_phones"></div>' );
 	$('header .logo').prepend('<div id="in_logo"></div>');
-	$('h1').prepend('<div id="in_title"></div>');
+	$('.banner__info__title').prepend('<div id="in_title"></div>');
 	$('footer').append('<div class="polygon_outer"><div class="polygon_inner"></div></div>');
 	$('header img').after('<div class="divider"></div>');
+	$('.banner__info__descr-cont__descr').after('<div class="shape"></div>');
 })
