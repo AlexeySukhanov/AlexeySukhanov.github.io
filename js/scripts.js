11f7c3a834b55bf0c7880a6820b666180e9c8faa
sukhanov <webdevforbiz@gmail.com>
2019-09-06 17:23:17 +0300

    footer bug fix

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,13 @@ $(document).ready(function(){
 	$('.phones').prepend( '<div id="in_phones"></div>' );
 	$('header .logo').prepend('<div id="in_logo"></div>');
 	$('.banner__info__title').prepend('<div id="in_title"></div>');
-
-
-
-	$('footer').append('<div class="polygon_outer"><div class="polygon_inner"></div></div>');
-
-
 	$('header img').after('<div class="divider"></div>');
 	$('.banner__info__descr-cont__descr').after('<div class="shape"></div>');
 
 	var user = detect.parse(navigator.userAgent);
-	console.log(user.browser.family);
-	console.log(user.browser.name);
+	console.log(user.browser.family === 'Edge');
+	if (user.browser.family !== 'Edge') {
+		$('footer').append('<div class="polygon_outer"><div class="polygon_inner"></div></div>');
+	}
+
 })
