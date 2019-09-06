2ce50679f5603300a2903337e65ca43dec8711e8
sukhanov <webdevforbiz@gmail.com>
2019-09-06 16:34:03 +0300

    test msie

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,13 @@ $(document).ready(function(){
 	$('.phones').prepend( '<div id="in_phones"></div>' );
 	$('header .logo').prepend('<div id="in_logo"></div>');
 	$('.banner__info__title').prepend('<div id="in_title"></div>');
+
+	if($.browser.msie){
+		alert('lol');
+	}
 	$('footer').append('<div class="polygon_outer"><div class="polygon_inner"></div></div>');
+
+
 	$('header img').after('<div class="divider"></div>');
 	$('.banner__info__descr-cont__descr').after('<div class="shape"></div>');
 })
